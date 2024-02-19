ddf2127c8777781de64f2d5f8dfa2fa59bc6dbca
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-02-19 10:43:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MESSAGE_CONTENT,
-
+    Intents.FLAGS.MESSAGE_CONTENT,
   ],
 });
 
