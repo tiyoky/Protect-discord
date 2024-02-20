1e05cabd5de3684c8d9295295f89f73c8f0a3748
tiyoky <160074145+tiyoky@users.noreply.github.com>
2024-02-20 10:47:32 -0500

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createserver(function (req, res) {
-   res.write("im alive");
+http.createServer(function (req, res) {
+   res.write("I'm alive");
    res.end();
 }).listen(8080);
